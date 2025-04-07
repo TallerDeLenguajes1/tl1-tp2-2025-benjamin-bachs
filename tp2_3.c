da86599d8b3440b9daf8be37e9c99d611dd88fd6
Benjamin Bachs <benjamin.bachs@gmail.com>
2025-04-07 10:48:29 -0300

    punto 3

---
+++ tp2_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5 
+#define M 7
+
+int main(int argc, char const *argv[])
+{
+    int i,j; 
+    int mt[N][M];
+    for(i = 0;i<N; i++)
+    {  
+        for(j = 0;j<M; j++)
+        { 
+            *(*(mt+i)+j)=1+rand()%100;
+            printf("%d   ", *(*(mt+i)+j));
+        } 
+        printf("\n"); 
+    } 
+    return 0;
+}
